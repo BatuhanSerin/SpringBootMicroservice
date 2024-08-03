48f1d815d61d87d8b8dfb80523d389b00eae3740
batuhanserin <batuhanserin97@gmail.com>
2024-08-03 10:59:27 +0300

    add retry event and time limiter to check stock via resilience4j

--- basket-service/src/main/java/com/example/basketService/service/BasketService.java
+++ basket-service/src/main/java/com/example/basketService/service/BasketService.java
@@ -10,21 +10,27 @@ import com.example.basketService.dto.basketResponse;
 import com.example.basketService.repository.BasketRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class BasketService {
     @Autowired
     private final BasketRepository basketRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<basketResponse> checkStock(List<String> productCode) {
+        log.info("Checking stock for product codes: {}", productCode);
+        Thread.sleep(10000);
+        log.info("Checked stock for product codes: {}", productCode);
         return basketRepository.findByProductCodeIn(productCode).stream()
-        .map(basket ->
-            basketResponse.builder().productCode(basket.getProductCode())
-            .isInStock(basket.getQuantity()>0).build()
-        ).toList();
+                .map(basket -> basketResponse.builder().productCode(basket.getProductCode())
+                        .isInStock(basket.getQuantity() > 0).build())
+                .toList();
     }
 
 }
--- order-service/src/main/java/com/example/orderService/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderService/controller/OrderController.java
@@ -1,5 +1,10 @@
 package com.example.orderService.controller;
 
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeoutException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +17,13 @@ import com.example.orderService.dto.OrderRequest;
 import com.example.orderService.service.OrderService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/order")
+@Slf4j
 public class OrderController {
 
     @Autowired
@@ -23,11 +32,17 @@ public class OrderController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackBasketService")
-    public String createOrderAPI(@RequestBody OrderRequest orderRequest) {
-        return orderService.createOrder(orderRequest);
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> createOrderAPI(@RequestBody OrderRequest orderRequest) {
+        // return orderService.createOrder(orderRequest);
+        return CompletableFuture.supplyAsync(() -> orderService.createOrder(orderRequest));
     }
 
-    public String fallbackBasketService(OrderRequest orderRequest, Throwable t) {
-        return "Fallback response:: Basket service is down. Please try again later";
+    public CompletableFuture<String> fallbackBasketService(OrderRequest orderRequest,
+            RuntimeException runtimeException) {
+        log.info("Fallback method:: Basket service is down. Please try again later" + LocalTime.now());
+        return CompletableFuture
+                .supplyAsync(() -> "Fallback response:: Basket service is down. Please try again later");
     }
 }
